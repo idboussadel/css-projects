030864e4707978b588ba15e3db86d8532310aef0
abdessamad idboussadel <idbouid@gmail.com>
2023-09-21 14:47:50 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// js for the btn toggle of the nav
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
